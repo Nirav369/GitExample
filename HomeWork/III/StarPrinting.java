0e75d3e993c75482ddaa4c5cf7da52a3bf7d6d50
Rithin <venkata9695@gmail.com>
2019-12-15 21:02:47 -0500

    HomeWork Submitted by --> Rithin

---
+++ HomeWork/III/StarPrinting.java
@@ -0,0 +1,16 @@
+package HomeWork.III;
+
+public class StarPrinting {
+	
+	public static void main(String[] args) {
+		
+		PrintTimes obj1 = new PrintTimes();
+		
+		
+		obj1.fourTimesStar("**********");
+		
+		obj1.addRight("*****");
+		
+		obj1.addLeft("*****");
+	}
+}
---
+++ HomeWork/V/Main.java
@@ -0,0 +1,15 @@
+package HomeWork.V;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		VowelFinder obj = new VowelFinder();
+		
+		//System.out.println(obj.isVowel('z'));
+		
+		obj.countVowels();
+		
+	}
+
+}
