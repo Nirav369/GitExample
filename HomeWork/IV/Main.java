36e581f032a52c7139c1d44fcb3e9667596584ba
Rithin <venkata9695@gmail.com>
2019-12-15 21:55:12 -0500

    Reset from rithin

---
+++ HomeWork/IV/Main.java
@@ -0,0 +1,12 @@
+package HomeWork.IV;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		SumOfTwoIntegers obj = new SumOfTwoIntegers();
+		
+		obj.getTotal(2, 3);
+	}
+
+}
---
+++ HomeWork/IV/SumOfTwoIntegers.java
@@ -0,0 +1,13 @@
+package HomeWork.IV;
+
+public class SumOfTwoIntegers {
+	
+	public void getTotal(int num1, int num2) {
+		
+		int sum = num1 + num2;
+		
+		System.out.println("Sum of "+num1+" and "+num2+" is "+sum);
+		
+	}
+
+}
